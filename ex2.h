2ba377e5a4f97d139cf65db19526e168f0673873
eyalgolan <eyalgolan96@gmail.com>
2019-11-26 17:54:06 +0200

    semi working insert function

--- ex2.h
+++ ex2.h
@@ -32,35 +32,37 @@ template <class T> class CacheManager {
     return (cacheMap.size() + 1 > size);
   }
   void updatePriority(string key, T obj) {
-    orderKeys.remove(key);
-    orderKeys.push_front(key);
-    this->cacheMap.erase(key);
-    list<string>::iterator order = orderKeys.begin();
-    cacheMap.insert(pair<string, pair<T, list<string>::iterator>>(key, pair<T, list<string>::iterator>(obj, order)));
+    orderKeys.remove(key);                                                      //remove key from LRU list
+    orderKeys.push_front(key);                                                  //push key to start of LRU list
+    this->cacheMap.erase(key);                                                  //erase it from cache
+    list<string>::iterator order = orderKeys.begin();                           //get pointer to start of LRU list
+    cacheMap.insert(pair<string, pair<T, list<string>::iterator>>(key,
+        pair<T, list<string>::iterator>(obj, order)));                          //enter to cache, with pointer to start of LRU list
   }
-  void writeToFile(string key, T obj) {
-    string filename = "../" + key + typeid(obj).name() + ".txt";
-    fstream out_file {filename,ios::out | ios::binary};                         //write to file BINARY
-    out_file.write((char *)&obj, sizeof(obj));
+  void writeToFile(string key, T obj) {                                         //write to file BINARY
+    string filename = "../" + key + typeid(obj).name() + ".txt";                //build filename
+    fstream out_file {filename,ios::out | ios::binary};                         //create file
+    out_file.write((char *)&obj, sizeof(obj));                                  //write data to file
   }
   void createItem(string key, T obj) {
-    orderKeys.push_front(key);
+    orderKeys.push_front(key);                                                  //push key to start of LRU list
     list<string>::iterator order = orderKeys.begin();
-    cacheMap.insert(pair<string, pair<T, list<string>::iterator>>(key, pair<T, list<string>::iterator>(obj, order)));
+    cacheMap.insert(pair<string, pair<T, list<string>::iterator>>(key,
+        pair<T, list<string>::iterator>(obj, order)));                          //enter to cache
     writeToFile(key, obj);                                                      //write to file BINARY
   }
   void removeLastFromCache() {
-    string key = orderKeys.back();
-    cacheMap.erase(key);
-    orderKeys.remove(key);
+    string key = orderKeys.back();                                              //get key of last member in LRU
+    cacheMap.erase(key);                                                        //erase it from cache
+    orderKeys.remove(key);                                                      //erase it from LRU list
   }
   void insert(string key, T obj) {
     if(isInCache(key)) {                                                        //if already in cache -> only need to update priority
-      updatePriority(key, obj);
+      updatePriority(key, obj);                                                 //update priority
     }
     else{                                                                       //if not in cache
       if(sizeIsOk()) {                                                          //and if cache is smaller then limit -> insert new item
-        createItem(key, obj);
+        createItem(key, obj);                                                   //insert new item
       }
       else {                                                                    //if not in cache
         writeToFile(key, obj);                                                  //write to file (even if in file, override)
@@ -73,9 +75,10 @@ template <class T> class CacheManager {
 //
 //  }
 //
-//  void foreach() {
-//
-//  }
+
+  void foreach() {
+
+  }
 
   CacheManager (int capacity) {
     this->size = capacity;
